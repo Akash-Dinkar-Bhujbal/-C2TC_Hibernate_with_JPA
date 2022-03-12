8e1eb389b8cf86e405396750ea697076031ac17e
Akash-Dinkar-Bhujbal <akashbhujbal97@gmail.com>
2022-03-12 19:16:23 +0530

    Fifth Commit: JoinedInheritance

---
+++ JPA_JoinedMapping/src/com/cg/jpastart/entities/Manager.java
@@ -0,0 +1,23 @@
+package com.cg.jpastart.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "mgr1")
+public class Manager extends Employee {
+
+	private static final long serialVersionUID = 1L;
+
+//	departmentName
+	private String departmentName;
+
+	public String getDepartmentName() {
+		return departmentName;
+	}
+
+	public void setDepartmentName(String departmentName) {
+		this.departmentName = departmentName;
+	}
+
+}
