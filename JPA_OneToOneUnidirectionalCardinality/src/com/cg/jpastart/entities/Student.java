aa5c9184c9c60ab61c93e214685628a7847935da
Akash-Dinkar-Bhujbal <akashbhujbal97@gmail.com>
2022-03-15 01:40:27 +0530

    Sixth Commit : OneToOne Unidirectional Cardinality

---
+++ JPA_OneToOneUnidirectionalCardinality/src/com/cg/jpastart/entities/Student.java
@@ -0,0 +1,56 @@
+package com.cg.jpastart.entities;
+
+import java.io.Serializable;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "student1")
+public class Student implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+//	studentId
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int studentId;
+
+	public int getStudentId() {
+		return studentId;
+	}
+
+	public void setStudentId(int studentId) {
+		this.studentId = studentId;
+	}
+
+//	name
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+//	address
+	@OneToOne(cascade = CascadeType.ALL)
+	@JoinColumn(name = "address_id")
+	private Address address;
+
+	public Address getAddress() {
+		return address;
+	}
+
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+
+}
